c277d11860b5ef0f9ba6e523a09fad6173369934
soumyadip007 <soumyadip.note@gmail.com>
2019-01-31 22:22:22 +0530

    Version 2.4

--- Aim-India-Foundation-WebDev-Internship/src/Foundation/RequestPage.java
+++ Aim-India-Foundation-WebDev-Internship/src/Foundation/RequestPage.java
@@ -1,12 +1,35 @@
 package Foundation;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
+import java.beans.Statement;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javafx.print.PrintColor;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
 /**
  * Servlet implementation class RequestPage
  */
@@ -19,7 +42,28 @@ public class RequestPage extends HttpServlet {
 		String name=request.getParameter("name");
 		String body=request.getParameter("body");
 		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
 		
+		Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/aim","root","");
+	         		
+		
+		PreparedStatement st=con.prepareStatement("INSERT INTO request(name,email,body) VALUES(?,?,?)");
+		st.setString(1,name);
+		st.setString(2,email);
+		st.setString(3, body);
+		
+		int i=0;
+		i=st.executeUpdate();
+		response.sendRedirect("index.html");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		System.out.println(name+email+body);
 	}
 
